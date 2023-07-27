e8b0e2bfaf4fbcebf1be445d3d8f0b0c5ca85277
Pablo Madrigal <pablomadrigal.1995@gmail.com>
2023-07-26 22:47:46 -0600

    Initial commit

---
+++ lib/shared/data/local_video_posts.dart
@@ -0,0 +1,63 @@
+List<Map<String, dynamic>> videoPosts = [
+  {
+    'name': 'Subiendo escaleras automáticas',
+    'videoUrl': 'assets/videos/1.mp4',
+    'likes': 23230,
+    'views': 1523,
+  },
+  {
+    'name': 'Perdida en el bosque',
+    'videoUrl': 'assets/videos/2.mp4',
+    'likes': 24230,
+    'views': 1343,
+  },
+  {
+    'name': 'A comprar!',
+    'videoUrl': 'assets/videos/3.mp4',
+    'likes': 21564320,
+    'views': 123563,
+  },
+  {
+    'name': 'El COVID no me afecta',
+    'videoUrl': 'assets/videos/4.mp4',
+    'likes': 3230,
+    'views': 31030,
+  },
+  {
+    'name': '¿Ser o no ser? que interesante',
+    'videoUrl': 'assets/videos/5.mp4',
+    'likes': 320,
+    'views': 2300,
+  },
+  {
+    'name': 'Maravillosa Jugada',
+    'videoUrl': 'assets/videos/6.mp4',
+    'likes': 10,
+    'views': 330,
+  },
+  {
+    'name': 'Estar conectados',
+    'videoUrl': 'assets/videos/7.mp4',
+    'likes': 1320,
+    'views': 33032,
+  },
+  {
+    'name': 'A cuidarnos ...',
+    'videoUrl': 'assets/videos/8.mp4',
+    'likes': 342,
+    'views': 3332,
+  },
+  {
+    'name': '¿Esto es trigo? que interesante',
+    'videoUrl': 'assets/videos/9.mp4',
+    'likes': 111,
+    'views': 2222,
+  },
+  {
+    'name': '¿Ya llegamos a la India?... umm si',
+    'videoUrl': 'assets/videos/10.mp4',
+    'likes': 0,
+    'views': 4,
+  },
+
+];
